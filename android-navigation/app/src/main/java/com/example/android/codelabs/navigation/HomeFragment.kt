43ceeec5b069828237059055bb293c88a0a392ee
taehee28 <dev.thk28@gmail.com>
2023-02-11 20:19:19 +0900

    Step. 7 Navigate using actions까지 진행

--- android-navigation/app/src/main/java/com/example/android/codelabs/navigation/HomeFragment.kt
+++ android-navigation/app/src/main/java/com/example/android/codelabs/navigation/HomeFragment.kt
@@ -18,7 +18,11 @@ package com.example.android.codelabs.navigation
 
 import android.os.Bundle
 import android.view.*
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.navOptions
 
 /**
  * Fragment used to show how to navigate to another destination
@@ -36,31 +40,40 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        //TODO STEP 5 - Set an OnClickListener, using Navigation.createNavigateOnClickListener()
-//        val button = view.findViewById<Button>(R.id.navigate_destination_button)
-//        button?.setOnClickListener {
-//            findNavController().navigate(R.id.flow_step_one_dest, null)
-//        }
+        //TODO STEP 5 - OnClickListener 설정하기. Navigation.createNavigateOnClickListener()을 사용할 수도 있다.
+        /*val button = view.findViewById<Button>(R.id.navigate_destination_button)
+        button?.setOnClickListener {
+            findNavController().navigate(R.id.flow_step_one_dest, null)
+        }*/
+
+        /*
+        // setOnClickListener에 람다를 넘기지 않고
+        // OnClickListener의 인스턴스를 만들어 넘길 수도 있다
+        button?.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.flow_step_one_dest, null)
+        )
+        */
+
         //TODO END STEP 5
 
-        //TODO STEP 6 - Set NavOptions
-//        val options = navOptions {
-//            anim {
-//                enter = R.anim.slide_in_right
-//                exit = R.anim.slide_out_left
-//                popEnter = R.anim.slide_in_left
-//                popExit = R.anim.slide_out_right
-//            }
-//        }
-//        view.findViewById<Button>(R.id.navigate_destination_button)?.setOnClickListener {
-//            findNavController().navigate(R.id.flow_step_one_dest, null, options)
-//        }
+        //TODO STEP 6 - NavOptions 설정하기
+        val options = navOptions {
+            anim {
+                enter = R.anim.slide_in_right
+                exit = R.anim.slide_out_left
+                popEnter = R.anim.slide_in_left
+                popExit = R.anim.slide_out_right
+            }
+        }
+        view.findViewById<Button>(R.id.navigate_destination_button)?.setOnClickListener {
+            findNavController().navigate(R.id.flow_step_one_dest, null, options)
+        }
         //TODO END STEP 6
 
-        //TODO STEP 7.2 - Update the OnClickListener to navigate using an action
-//        view.findViewById<Button>(R.id.navigate_action_button)?.setOnClickListener(
-//                Navigation.createNavigateOnClickListener(R.id.next_action, null)
-//        )
+        //TODO STEP 7.2 - action을 사용해서 navigate 하도록 업데이트
+        view.findViewById<Button>(R.id.navigate_action_button)?.setOnClickListener(
+                Navigation.createNavigateOnClickListener(R.id.next_action, null)
+        )
         //TODO END STEP 7.2
     }
 
