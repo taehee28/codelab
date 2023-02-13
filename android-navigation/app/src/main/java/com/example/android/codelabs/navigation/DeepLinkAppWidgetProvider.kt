d0b2c5a47d58048f7c17b72a7c70d68c3f662e34
taehee28 <dev.thk28@gmail.com>
2023-02-13 12:54:17 +0900

    Step. 11 Associating a web link with a destination까지 진행

--- android-navigation/app/src/main/java/com/example/android/codelabs/navigation/DeepLinkAppWidgetProvider.kt
+++ android-navigation/app/src/main/java/com/example/android/codelabs/navigation/DeepLinkAppWidgetProvider.kt
@@ -21,6 +21,7 @@ import android.appwidget.AppWidgetProvider
 import android.content.Context
 import android.os.Bundle
 import android.widget.RemoteViews
+import androidx.navigation.NavDeepLinkBuilder
 
 /**
  * App Widget that deep links you to the [DeepLinkFragment].
@@ -38,14 +39,23 @@ class DeepLinkAppWidgetProvider : AppWidgetProvider() {
 
         val args = Bundle()
         args.putString("myarg", "From Widget")
-        // TODO STEP 10 - construct and set a PendingIntent using DeepLinkBuilder
-//        val pendingIntent = NavDeepLinkBuilder(context)
-//                .setGraph(R.navigation.mobile_navigation)
-//                .setDestination(R.id.deeplink_dest)
-//                .setArguments(args)
-//                .createPendingIntent()
-//
-//        remoteViews.setOnClickPendingIntent(R.id.deep_link_button, pendingIntent)
+        // TODO STEP 10 - DeepLinkBuilder를 사용해서 PendingIntent 구성하기
+        // 기본적으로 NavDeepLingBuilder는 launcher activity를 실행시킨다.
+        // 실행하고자 하는 activity의 context를 넘기거나, setComponentName 메서드에 activity 이름을 넘기면 오버라이딩 가능
+
+        // deep link를 실행할 때 backstack은 넘기는 navigation graph에 의해 결정된다.
+        // 만약 지정한 activity가 부모 activity를 가진다면 부모 activity들 또한 backstack에 포함된다.
+        // app:startDestination을 통해 지정된 destination들을 사용해 backstack이 생성된다.
+        // 복잡한 경우 nested navigation graph를 포함할 수 있는데,
+        // 각 nested navigation graph에 설정된 app:startDestination 속성이 backstack을 결정한다.
+
+        val pendingIntent = NavDeepLinkBuilder(context)
+                .setGraph(R.navigation.mobile_navigation)
+                .setDestination(R.id.deeplink_dest)
+                .setArguments(args)
+                .createPendingIntent()
+
+        remoteViews.setOnClickPendingIntent(R.id.deep_link_button, pendingIntent)
         // TODO END STEP 10
         appWidgetManager.updateAppWidget(appWidgetIds, remoteViews)
     }
